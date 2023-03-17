8d808e6cf43115dea4175d1a91c4551e946cbb48
Dwayne Reed <dreedjr1992@gmail.com>
2023-03-17 11:29:37 -0500

    2.2 commit

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 const APIKEY = '04c35731a5ee918f014970082a0088b1';
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280/';0456   
+async function getMovies() {
+   const resp = await fetch (APIURL);
+   const respData = await resp.json();
+
+   console.log(respData)
+
+   respData.results.forEach(movie => {
+     const {poster_path, title, vote_average
+     } = movie;
+
+      const movieE1 = document.createElement
+      ('div');
+      movieE1.classList.add('movie');
+
+      movieE1.innerHTML = `
+            >
+      <img 
+      src="${IMGPATH + poster_path}" 
+      alt="${title}"
+      />
+   <div class="movie-info"> 
+        <h3>${title}</h3>
+        <span>${vote_average}</span>
+   </div>
+  </div> 
+  
+  `
+  
+      document.body.appendChild(movieE1);
+   });
+
+
+   
+   return respData;
+
+}
+
+getMovies();
